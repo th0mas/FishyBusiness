2a3c0b32eb7e264387d0fec06127bf8835eb9619
Tom Haines <tom@tomhaines.xyz>
2021-03-20 23:38:57 +0000

    Include users

--- fishy_business/lib/fishy_business/game/manager.ex
+++ fishy_business/lib/fishy_business/game/manager.ex
@@ -23,12 +23,13 @@ defmodule FishyBusiness.Game.Manager do
         types: ["haddock", "salmon"],
         active: []
       },
-    ],
-    me: %{
-      money: 100,
-      bait: 10,
-      items: []
-    }
+    ]
+  }
+
+  @initial_player %{
+    money: 100,
+    bait: 10,
+    items: []
   }
 
   # Initial State:
@@ -41,17 +42,31 @@ defmodule FishyBusiness.Game.Manager do
     GenServer.start_link(__MODULE__, state, name: name)
   end
 
-  def init(%{game: game, players: _players} = state) do
-    broadcast!(game, "init_game", @initial_state)
+  def init(%{game: game, players: players} = state) do
+    broadcast!(game, "init_game", gen_initial_state(players))
     send(self(), :timed_event)
     Logger.info("Game manager inited for" <> game)
+
+
+    state = state |>
+      Map.put(:current, @initial_state)
     {:ok, state}
   end
 
-  def handle_in(:timed_event, %{game: game} = state) do
+  def handle_info(:timed_event, %{game: game} = state) do
     broadcast!(game, "timed_event", %{})
     Process.send_after(self(), :timed_event, 1000 * 60 * 2)
-    {:ok, state}
+    {:noreply, state}
+  end
+
+  def gen_initial_state(players) do
+    Logger.info inspect(players)
+
+    @initial_state
+    |> Map.put(:players, Enum.map(players,
+      fn {k, v} -> Map.merge(@initial_player, %{name: v |> Map.get(:metas) |> List.first |> Map.get(:name), id: k}) end)
+    )
+    |> Map.put(:me, @initial_player)
   end
 
 
