fbb3565de2f54c884aa7ccef33bde54e8dd2e240
Joel Grimmer <joel.grimmer@gmail.com>
2021-03-20 17:57:08 +0000

    .gitignore stuff

--- frontend/src/services/api/requestGameToken.js
+++ frontend/src/services/api/requestGameToken.js
@@ -1,5 +1,9 @@
 const requestGameToken = async (gameName) => {
-  const res = await fetch(`http://localhost:4000/api/rooms/${gameName}`);
+  const res = await fetch(`http://localhost:4000/api/rooms/${gameName}`,
+    {
+      mode: 'cors'
+    }
+  );
   if (res.ok) {
     return res.json().token;
   } else {
