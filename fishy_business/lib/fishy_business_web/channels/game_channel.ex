9a0749ea43b69917f956c537659cbc0ff3e44fa8
Tom Haines <tom@tomhaines.xyz>
2021-03-20 15:54:09 +0000

    Created game channel

---
+++ fishy_business/lib/fishy_business_web/channels/game_channel.ex
@@ -0,0 +1,14 @@
+defmodule FishyBusinessWeb.GameChannel do
+  use FishyBusinessWeb, :channel
+
+  alias FishyBusiness.Game
+  alias FishyBusiness.Game.Room
+
+  def join("game:" <> game_slug, %{"token" => token}, socket) do
+    game = Game.find_room_by_slug(game_slug)
+    case Phoenix.Token.verify(socket, "room", token) do
+      {:ok, _} -> {:ok, assign(socket, :game_id, game.id)}
+      {:error, _} -> :error
+    end
+  end
+end
