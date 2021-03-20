cf45e987fc3d090e2265f9111d8289179067b5dd
Joel Grimmer <joel.grimmer@gmail.com>
2021-03-20 20:54:18 +0000

    css

--- frontend/src/pages/Lobby.js
+++ frontend/src/pages/Lobby.js
@@ -7,18 +7,18 @@ function Lobby({ state, gameCode, updateplayername }) {
   const history = useHistory();
 
   return (
-    <div className="lobby">
-      <h1 className="">lobby</h1>
-      <button onClick={() => history.push("/game/play")} className="">start</button>
-      <button className="">leave</button>
-      <div className="playername">
-        <input className="border-2 p-1 mr-1 rounded-md border-blue-300" onChange={(e) => setName(e.target.value)} value={name} />
-        <button className="border-2 p-1 rounded-md border-indigo-300" onClick={() => updateplayername(name)}>set name</button>
+    <div className="lobby h-screen p-10 px-12 bg-gradient-to-r from-blue-500 via-blue-400 to-green-200">
+      <h1 className="m-2 py-4 text-4xl text-white font-semibold">lobby</h1>
+      <button onClick={() => history.push("/game/play")} className="p-2 m-2 text-white text-xl bg-green-400 rounded-md">start</button>
+      <button onClick={() => history.push("/")} className="p-2 text-white text-xl bg-red-500 rounded-md">leave</button>
+      <div className="playername m-2">
+        <input className="p-2 mr-1 rounded-md" onChange={(e) => setName(e.target.value)} value={name} />
+        <button className="text-white p-2 rounded-md bg-blue-500" onClick={() => updateplayername(name)}>set name</button>
       </div>
-      <div>
+      <div className="m-2 text-white">
         <p>code: {gameCode}</p>
       </div>
-      <div className="players">
+      <div className="players m-2 text-white">
         <h3>opponents:</h3>
         {
           state.players.map((p, index) => {
