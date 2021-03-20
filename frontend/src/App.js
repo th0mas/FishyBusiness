6a555472dff2ae2774a26822798fa0a0ff7a717a
Joel Grimmer <joel.grimmer@gmail.com>
2021-03-20 12:40:45 +0000

    Basic structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Play from './pages/Play';
+import Home from './pages/Home';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Router path="/play">
+            <Play />
+          </Router>
+          <Router path="/">
+            <Home />
+          </Router>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Play.js
@@ -0,0 +1,9 @@
+function Play() {
+    return (
+        <div className="play">
+            <h1>Play</h1>
+        </div>
+    );
+}
+
+export default Play;
