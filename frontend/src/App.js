f38b71273916803810bdf414780ae5ff51ddb7f6
Joel Grimmer <joel.grimmer@gmail.com>
2021-03-20 12:46:00 +0000

    unbreak things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
 import Play from './pages/Play';
 import Home from './pages/Home';
-import { BrowserRouter, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Switch>
-          <Router path="/play">
-            <Play />
-          </Router>
-          <Router path="/">
-            <Home />
-          </Router>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/play">
+          <Play />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
