1e6839cc6b55dd8a87428d9d9e8a9d5e25dd2ff3
ConnorCairns <connor.f.cairns@gmail.com>
2021-03-21 09:39:02 +0000

    things

--- frontend/src/components/DropdownMenu.js
+++ frontend/src/components/DropdownMenu.js
@@ -1,8 +1,11 @@
 const DropdownMenu = ({ gameState, handleClick, region }) => {
+  let fishingItems = gameState.me.items.filter(item => item.name !== "Oil Spill")
+
   return (
-    <div className="group inline-block w-max">
+    <div className="flex">
+    <div className="group inline-block self-center pr-2">
       <button
-        className="outline-none cursor-auto focus:outline-none border px-3 py-1 bg-green-400 rounded-sm flex items-center min-w-32"
+        className="outline-none cursor-auto focus:outline-none border px-3 py-1 hover:bg-green-500 bg-green-400 rounded-sm flex items-center min-w-32"
       >
         <span className="pr-1 font-semibold text-white flex-1">Fish</span>
         <span>
@@ -22,14 +25,15 @@ const DropdownMenu = ({ gameState, handleClick, region }) => {
   transition duration-150 ease-in-out origin-top min-w-32"
       >
         {
-          gameState.me.items.length === 0 && <li className="rounded-sm px-3 py-1 text-gray bg-gray-100">purchase items</li>
+          fishingItems.length === 0 && <li className="rounded-sm px-3 py-1 text-gray bg-gray-100">purchase items</li>
         }
         {
-          gameState.me.items.map((item, index) =>
+          fishingItems.map((item, index) =>
             <li key={index} onClick={() => handleClick(item, region)} className="rounded-sm cursor-pointer px-3 py-1 hover:bg-gray-100">{item.name}</li>
           )
         }
       </ul>
+    </div>
     </div>
   )
 }
