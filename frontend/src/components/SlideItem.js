991ff75d5012fd22474c1559bbb7c6d6fb98f157
Joel Grimmer <joel.grimmer@gmail.com>
2021-03-21 10:01:25 +0000

    removed css warnings

--- frontend/src/components/SlideItem.js
+++ frontend/src/components/SlideItem.js
@@ -40,14 +40,14 @@ const SlideItem = ({ item, me, shop }) => {
                 <p>£{item.price}</p>
               </div>
               <div className="flex items-center justify-end">
-                <button onClick={() => addItem(me, item, dispatch, setError)} className="right-0 bg-green-500 text-white px-3 py-2 hover:bg-green-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Buy</button>
+                <button onClick={() => addItem(me, item, dispatch, setError)} className="right-0 bg-green-500 text-white px-3 py-2 hover:bg-green-800 hover:text-white rounded-md text-sm font-medium">Buy</button>
               </div>
             </div>
           }
         </div>
       </div>
       {error &&
-        <div className="bg-red-200 px-6 py-4 mx-2 my-4 rounded-md text-lg flex items-center mx-auto w-3/4 xl:w-2/4">
+        <div className="bg-red-200 px-6 py-4 my-4 rounded-md text-lg flex items-center mx-auto w-3/4 xl:w-2/4">
           <svg viewBox="0 0 24 24" className="text-red-600 w-5 h-5 sm:w-5 sm:h-5 mr-3">
             <path fill="currentColor"
               d="M11.983,0a12.206,12.206,0,0,0-8.51,3.653A11.8,11.8,0,0,0,0,12.207,11.779,11.779,0,0,0,11.8,24h.214A12.111,12.111,0,0,0,24,11.791h0A11.766,11.766,0,0,0,11.983,0ZM10.5,16.542a1.476,1.476,0,0,1,1.449-1.53h.027a1.527,1.527,0,0,1,1.523,1.47,1.475,1.475,0,0,1-1.449,1.53h-.027A1.529,1.529,0,0,1,10.5,16.542ZM11,12.5v-6a1,1,0,0,1,2,0v6a1,1,0,1,1-2,0Z"></path>
